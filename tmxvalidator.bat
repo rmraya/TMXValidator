0a2bf07233b54713af7bfcb345694a7e516f6633
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-02-05 14:52:01 -0300

    Updated build code

--- tmxvalidator.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pushd "%~dp0" 
-bin\java.exe --module-path lib -m tmxvalidator/com.maxprograms.tmxvalidation.TMXValidator %* 
--- tmxvalidator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/"
-
-bin/java --module-path lib -m tmxvalidator/com.maxprograms.tmxvalidation.TMXValidator $@
