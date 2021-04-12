92ba7d6cdf945235dad4f55a0bc51b40bbe8a8b1
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-04-12 14:58:54 -0300

    Updated libraries

--- src/com/maxprograms/tmxvalidation/Constants.java
+++ src/com/maxprograms/tmxvalidation/Constants.java
@@ -14,7 +14,7 @@ package com.maxprograms.tmxvalidation;
 public class Constants {
 
 	public static final String VERSION="2.2.0";
-	public static final String BUILD="20210102_0825";
+	public static final String BUILD="20210412_1454";
 
 	public static final String SUCCESS = "Success";
 	public static final String ERROR = "Error";
