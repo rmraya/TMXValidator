5d15f305f9f28cc663b4f83b4a65c14e082f1cc5
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-03-24 06:37:51 -0300

    Updated libraries

--- src/com/maxprograms/tmxvalidation/Constants.java
+++ src/com/maxprograms/tmxvalidation/Constants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005-2024 Maxprograms.
+ * Copyright (c) 2005-2025 Maxprograms.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 1.0
@@ -13,8 +13,8 @@ package com.maxprograms.tmxvalidation;
 
 public class Constants {
 
-	public static final String VERSION="2.7.0";
-	public static final String BUILD="20240322_0424";
+	public static final String VERSION="2.8.0";
+	public static final String BUILD="20250324_0632";
 
 	public static final String SUCCESS = "Success";
 	public static final String ERROR = "Error";
